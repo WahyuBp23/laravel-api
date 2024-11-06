f4c1cc841140f6fbcc4409d15fb1598eb7a6ebcb
Wahyu Bagus Pambudi <wb84222@gmail.com>
2024-11-06 20:39:20 +0700

    feat: :sparkles: Update all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Author;
-use App\Models\Book;
-use App\Models\Genre;
 use Illuminate\Database\Seeder;
-
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run() {
+    public function run(): void
+    {
+        // User::factory(10)->create();
 
-        
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
